1b472f22be95dfda2cb7484756b3297834b313ca
premshetty <premenaga@gmail.com>
2023-07-05 23:37:04 +0530

    path changed

--- ThemeProvider.js
+++ ThemeProvider.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export default function ThemeProvider({ children, ...props }) {
+export function ThemeProvider({ children, ...props }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/global.css";
-import { ThemeProvider } from "./ThemeProvider";
+import { ThemeProvider } from "../../ThemeProvider";
 
 export default function App({ Component, pageProps }) {
   return (
